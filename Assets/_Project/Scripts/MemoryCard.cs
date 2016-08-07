749dc6f2d1e9d32d5b5fe7e6c20c7dd548f6fd5b
John Jesus <john.j.jesus@gmail.com>
2016-08-07 14:43:14 -0500

    Flip card.

---
+++ Assets/_Project/Scripts/MemoryCard.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MemoryCard : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject cardBack;
+
+    [SerializeField]
+    private Sprite image;
+
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf)
+        {
+            cardBack.SetActive(false);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpriteRenderer>().sprite = image;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
