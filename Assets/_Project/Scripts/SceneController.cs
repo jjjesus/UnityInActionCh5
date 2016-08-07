e7ef57af090a19b964b858ce8d122f0eb6819914
John Jesus <john.j.jesus@gmail.com>
2016-08-07 15:21:56 -0500

    Instantiate multiple cards.

--- Assets/_Project/Scripts/SceneController.cs
+++ Assets/_Project/Scripts/SceneController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class SceneController : MonoBehaviour {
 
@@ -9,17 +10,51 @@ public class SceneController : MonoBehaviour {
     [SerializeField]
     private Sprite[] images;
 
+    public const int gridRows = 2;
+    public const int gridCols = 4;
+    public const float offsetX = 2f;
+    public const float offsetY = 2.5f;
+
 
 	// Use this for initialization
 	void Start () {
-        int id = Random.Range(0, images.Length);
-        string msg = string.Format("Setting id to {0}", id);
-        Debug.Log(msg);
-        originalCard.SetCard(id, images[id]);
+        createCards();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void createCards()
+    {
+        Vector3 startPos = originalCard.transform.position;
+
+        for (int col = 0; col < gridCols; col++)
+        {
+            for (int row = 0; row < gridRows; row++)
+            {
+                createNewCard(startPos, col, row);
+            }
+        }
+    }
+
+    private void createNewCard(Vector3 startPos, int col, int row)
+    {
+        MemoryCard card;
+        if (col == 0 && row == 0)
+        {
+            card = originalCard;
+        }
+        else
+        {
+            card = Instantiate(originalCard) as MemoryCard;
+        }
+        int id = UnityEngine.Random.Range(0, images.Length);
+        card.SetCard(id, images[id]);
+
+        float posX = (offsetX * col) + startPos.x;
+        float posY = -(offsetY * row) + startPos.y;
+        card.transform.position = new Vector3(posX, posY, startPos.z);
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 }
